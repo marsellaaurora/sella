728a4bc3eb1880f98598f064beca88cdb506658c
Marsella Aurora Agustin <m.aurora1808@gmail.com>
2020-02-13 09:35:53 +0700

    menampilkan data\

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,7 @@
 //app/config/db.config.js
 module.exports = {
-    HOST: "xxx",
-    USER: "xxx",
-    PASSWORD: "xxx",
-    DATABASE: "xxx"
-   };
-   
+ HOST: "xxx",
+ USER: "xxx",
+ PASSWORD: "xxx",
+ DATABASE: "xxx"
+};
---
+++ routers/book.routers.js
@@ -0,0 +1,9 @@
+//app/routes/book.routes.js
+module.exports = app => {
+    const books = require("../controllers/book.controller");
+    // cara mengakses gambar
+   http://polibatam.ac.id/img/perpustakaan.png
+    // Mengambil semua data
+    app.get("/api/books", books.findAll);
+   };
+   
